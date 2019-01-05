401dc78d1e691ef1099891a98c6b267b528380e0
Kwangwoo <rhkddn1657@gmail.com>
2019-01-05 15:47:29 +0900

    Ptj1 진행중 - 데이터바인딩 첫 사용 - ui 작업중

--- BoostCourse/app/src/main/java/com/example/kiyon/boostcourse/MainActivity.java
+++ BoostCourse/app/src/main/java/com/example/kiyon/boostcourse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kiyon.boostcourse;
 
 import android.databinding.DataBindingComponent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
+
+        InitView();
+    }
+
+    private void InitView() {
+
+        binding.toolbar.setTitle("씨네마천국");
+        binding.toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(binding.toolbar);
+
 
     }
 }
